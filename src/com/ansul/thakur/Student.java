e10520b4a5ba0a888f584f41cc337f92e2fe89e0
Ansul Thakur <ansultkr@iitk.ac.in>
2020-09-22 11:43:02 +0530

    3: @Value HardCoding added <context:annotation-config/>

--- src/com/ansul/thakur/Student.java
+++ src/com/ansul/thakur/Student.java
@@ -1,17 +1,21 @@
 package com.ansul.thakur;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class Student {
 	
 	private String name;
 	private String interestedCourse;
 	private String hobby;
-	
+	@Value("Ishan")
 	public void setName(String name) {
 		this.name = name;
 	}
+	@Value("machine Learning")
 	public void setInterestedCourse(String interestedCourse) {
 		this.interestedCourse = interestedCourse;
 	}
+	@Value("Tennis")
 	public void setHobby(String hobby) {
 		this.hobby = hobby;
 	}
