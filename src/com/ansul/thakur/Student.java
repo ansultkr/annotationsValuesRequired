2c02f6ae8874dd67058672b27c9628d36f0c27e2
Ansul Thakur <ansultkr@iitk.ac.in>
2020-09-22 11:24:30 +0530

    1: Basic Setup

---
+++ src/com/ansul/thakur/Student.java
@@ -0,0 +1,26 @@
+package com.ansul.thakur;
+
+public class Student {
+	
+	private String name;
+	private String interestedCourse;
+	private String hobby;
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setInterestedCourse(String interestedCourse) {
+		this.interestedCourse = interestedCourse;
+	}
+	public void setHobby(String hobby) {
+		this.hobby = hobby;
+	}
+	
+	public void displayStudentInfo() {
+		System.out.println("Student Name: "+name);
+		System.out.println("Student Course: "+interestedCourse);
+		System.out.println("Student Hobby: "+hobby);
+	}
+	
+
+}
